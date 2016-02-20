8fc378601c84e050adbeee61c2d8a887cfbec4a0
Nathan Ansel <nathan3o4@gmail.com>
2016-02-19 19:52:44 -0600

    Initial Commit

---
+++ NAPercentBar/NAPercentBar.h
@@ -0,0 +1,19 @@
+//
+//  NAPercentBar.h
+//  NAPercentBar
+//
+//  Created by Nathan Ansel on 2/19/16.
+//  Copyright © 2016 Nathan Ansel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NAPercentBar.
+FOUNDATION_EXPORT double NAPercentBarVersionNumber;
+
+//! Project version string for NAPercentBar.
+FOUNDATION_EXPORT const unsigned char NAPercentBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NAPercentBar/PublicHeader.h>
+
+

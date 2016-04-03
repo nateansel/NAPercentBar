b5ddaf594e63b7ccbf7a26fced9a7ccd722446c0
Nathan Ansel <nathan3o4@gmail.com>
2016-04-02 21:05:37 -0500

    Started the Example project
    Still running into an issue with the percentView not showing up but I will continue to work on it and will fix it soon.

---
+++ NAPercentBarExample/NAPercentBarExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NAPercentBarExample
+//
+//  Created by Nathan Ansel on 3/28/16.
+//  Copyright © 2016 Nathan Ansel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
